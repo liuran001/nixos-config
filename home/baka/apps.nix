cc74436f40e75f9b4a4bef854ea5903c9b247856
liuran001 <bdovo@bdovo.cc>
2026-08-19 04:37:45 +0800

    feat: 添加通讯与影音桌面应用

--- home/baka/apps.nix
+++ home/baka/apps.nix
@@ -3,11 +3,27 @@
 
 {
   home.packages = with pkgs; [
+    # 通讯、协作与远程桌面。
+    wechat
+    qq
+    telegram-desktop
+    feishu
+    wemeet
+    rustdesk-flutter
+
+    # 影音、音乐与直播。
+    (callPackage ../../pkgs/bilibili.nix { })
+    (callPackage ../../pkgs/douyin.nix { })
+    (callPackage ../../pkgs/open-orpheus.nix { })
+    (callPackage ../../pkgs/splayer-next.nix { })
+    go-musicfox
+    vlc
+    obs-studio
+
+    # 桌面工具和外观。
     fastfetch
     kdePackages.kate
-    telegram-desktop
     microsoft-edge
-    vlc
     flameshot
     (callPackage ../../pkgs/pjsk-cursor-theme.nix { })
   ];
